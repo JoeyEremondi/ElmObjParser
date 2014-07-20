626c014fa5bd39b2cb175d089d4185658883519b
JoeyEremondi <jse313@mail.usask.ca>
2014-07-20 11:56:09 -0600

    Add deploy script for tags

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mv elm_dependencies.json elm_deps
+cat elm_deps | tr  "\n" " " > elm_deps
+sed -i 's/\"dependencies\": {.*}/\"dependencies\":{}/g' elm_deps
+cp elm_deps elm_dependencies.json
+elm-get publish
+
+#end
