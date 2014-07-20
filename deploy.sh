e15e00b74b0f7d5187c394d970d80fd261757dfd
JoeyEremondi <jse313@mail.usask.ca>
2014-07-20 12:05:13 -0600

    Fix wrong binary call

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ mv elm_dependencies.json elm_deps
 cat elm_deps | tr  "\n" " " > elm_deps
 sed -i 's/\"dependencies\": {.*}/\"dependencies\":{}/g' elm_deps
 cp elm_deps elm_dependencies.json
-elm-get publish
+./elm-get publish
 
 #end
