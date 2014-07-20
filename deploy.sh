c309002602927ef56a4bd9e729a4aa8f2c4d6d20
JoeyEremondi <jse313@mail.usask.ca>
2014-07-20 12:28:00 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,10 @@ mv elm_dependencies.json elm_deps
 echo "done move"
 cat elm_deps | tr  "\n" " " > elm_deps
 echo "removed newlines"
+cat elm_deps
 sed -i 's/\"dependencies\": {[^}]*}/\"dependencies\":{}/g' elm_deps
 echo "replaced deps"
+cat elm_deps
 cp elm_deps elm_dependencies.json
 echo "copied file"
 cat elm_dependencies.json
