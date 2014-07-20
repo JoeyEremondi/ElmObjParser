5540d6fc5d312029b9fa4106f7dbe1704ebb89c7
JoeyEremondi <jse313@mail.usask.ca>
2014-07-20 12:11:43 -0600

    Try non-greedy regex match

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 mv elm_dependencies.json elm_deps
+echo "done move"
 cat elm_deps | tr  "\n" " " > elm_deps
-sed -i 's/\"dependencies\": {.*}/\"dependencies\":{}/g' elm_deps
+echo "removed newlines"
+sed -i 's/\"dependencies\": {[^}]*}/\"dependencies\":{}/g' elm_deps
+echo "replaced deps"
 cp elm_deps elm_dependencies.json
+echo "copied file"
 ./elm-get publish
+echo "did publish"
 
 #end
